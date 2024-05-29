6b223bf726315a3b7bb099907e674ee1dc8321f6
xiatiandeairen <384660495@qq.com>
2024-05-30 01:28:21 +0800

    update install.sh

--- zsh_config/scripts/alias.sh
+++ zsh_config/scripts/alias.sh
@@ -2,4 +2,10 @@
 alias zshconfig="nvim $ZSH_CONFIG_PATH/.zshrc"
 alias customconfig="nvim $BASE_CONFIG_PATH"
 
+# ls
 alias ls='eza --long --all --header --icons --git'
+alias cat="bat"
+
+#redis
+alias redis-startup="redis-server /usr/local/etc/redis.conf"
+alias redis-shutdown="redis-cli shutdown"
